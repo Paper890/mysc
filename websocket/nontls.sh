5b33a142a2adbe87a33cb501a62ecd33fb639975
Paper890 <140361640+Paper890@users.noreply.github.com>
2023-09-18 18:35:57 +0800

    Update nontls.sh

--- websocket/nontls.sh
+++ websocket/nontls.sh
@@ -16,7 +16,7 @@ red='\e[1;31m'
 green='\e[0;32m'
 yell='\e[1;33m'
 NC='\e[0m'
-echo "Installing SSH Websocket None TLS by RstoreVPN " | lolcat
+echo "Installing SSH Websocket None TLS by SAN " | lolcat
 echo "Progress..." | lolcat
 sleep 3
 
