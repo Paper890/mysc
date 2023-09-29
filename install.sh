afdba2a8b58e52d1bd3feb3138882ad638769bee
Paper890 <140361640+Paper890@users.noreply.github.com>
2023-09-29 18:55:25 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -387,9 +387,7 @@ echo "" | tee -a log-install.txt
 rm /root/cf.sh >/dev/null 2>&1
 rm /root/setup.sh >/dev/null 2>&1
 rm /root/insshws.sh 
-rm /root/update.sh
 rm /root/nontls.sh
-rm /root/install-sldns.sh
 secs_to_human "$(($(date +%s) - ${start}))" | tee -a log-install.txt
 echo -e "
 "
