7a9d512f3476e7391d86bb1187d435f021adb847
Paper890 <140361640+Paper890@users.noreply.github.com>
2023-10-19 09:26:37 +0800

    Update nontls.sh

--- websocket/nontls.sh
+++ websocket/nontls.sh
@@ -21,7 +21,7 @@ echo "Progress..." | lolcat
 sleep 3
 
 # Getting Proxy Template
-wget -q -O /usr/local/bin/ws-nontls https://raw.githubusercontent.com/myridwan/VVIP/ipuk/websocket/ws-nontls.py
+wget -q -O /usr/local/bin/ws-nontls https://raw.githubusercontent.com/Paper890/mysc/main/websocket/ws-nontls.py
 chmod +x /usr/local/bin/ws-nontls
 
 # Installing Service
