b837af8775d7af201976a57a864e3cf716eb725c
Paper890 <140361640+Paper890@users.noreply.github.com>
2023-10-26 08:17:37 +0800

    Update rebootvps.sh

--- corn/rebootvps.sh
+++ corn/rebootvps.sh
@@ -3,4 +3,4 @@ DATE=$(date +"%m-%d-%Y")
 TIME=$(date +"%T")
 rm -f /root/log-reboot.txt
 echo -e "Server was successfully rebooted on $DATE at $TIME." >> /root/log-reboot.txt
-#/sbin/shutdown -r now
+/sbin/shutdown -r now
