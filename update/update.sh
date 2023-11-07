565f50b70af6f03cb66e4cfd4b693b26c728d99e
Paper890 <140361640+Paper890@users.noreply.github.com>
2023-11-07 15:14:11 +0800

    Update update.sh

--- update/update.sh
+++ update/update.sh
@@ -9,22 +9,17 @@ wget -q -O /usr/bin/menu-ss "https://raw.githubusercontent.com/Paper890/mysc/mai
 wget -q -O /usr/bin/menu-vmess "https://raw.githubusercontent.com/Paper890/mysc/main/update/menu-vmess.sh" && chmod +x /usr/bin/menu-vmess
 wget -q -O /usr/bin/menu-vless "https://raw.githubusercontent.com/Paper890/mysc/main/update/menu-vless.sh" && chmod +x /usr/bin/menu-vless
 wget -q -O /usr/bin/menu-trojan "https://raw.githubusercontent.com/Paper890/mysc/main/update/menu-trojan.sh" && chmod +x /usr/bin/menu-trojan
-wget -q -O /usr/bin/menu-bot "https://raw.githubusercontent.com/Paper890/mysc/main/update/menu-bot.sh" && chmod +x /usr/bin/menu-bot
 wget -q -O /usr/bin/menu-ssh "https://raw.githubusercontent.com/Paper890/mysc/main/update/menu-ssh.sh" && chmod +x /usr/bin/menu-ssh
 wget -q -O /usr/bin/menu-set "https://raw.githubusercontent.com/Paper890/mysc/main/update/menu-set.sh" && chmod +x /usr/bin/menu-set
-wget -q -O /usr/bin/menu-theme "https://raw.githubusercontent.com/Paper890/mysc/main/update/menu-theme.sh" && chmod +x /usr/bin/menu-theme
 wget -q -O /usr/bin/menu-backup "https://raw.githubusercontent.com/Paper890/mysc/main/update/menu-backup.sh" && chmod +x /usr/bin/menu-backup
 wget -q -O /usr/bin/menu-ip "https://raw.githubusercontent.com/Paper890/mysc/main/update/menu-ip.sh" && chmod +x /usr/bin/menu-ip
 wget -q -O /usr/bin/menu-tor "https://raw.githubusercontent.com/Paper890/mysc/main/update/menu-tor.sh" && chmod +x /usr/bin/menu-tor
 wget -q -O /usr/bin/auto-reboot "https://raw.githubusercontent.com/Paper890/mysc/main/update/auto-reboot.sh" && chmod +x /usr/bin/auto-reboot
 wget -q -O /usr/bin/menu-tcp "https://raw.githubusercontent.com/Paper890/mysc/main/update/menu-tcp.sh" && chmod +x /usr/bin/menu-tcp
 wget -q -O /usr/bin/rebootvps "https://raw.githubusercontent.com/Paper890/mysc/main/corn/rebootvps.sh" && chmod +x /usr/bin/rebootvps
-wget -q -O /usr/bin/menu-dns "https://raw.githubusercontent.com/Paper890/mysc/main/update/menu-dns.sh" && chmod +x /usr/bin/menu-dns
 wget -q -O /usr/bin/info "https://raw.githubusercontent.com/Paper890/mysc/main/update/info.sh" && chmod +x /usr/bin/info
-wget -q -O /usr/bin/mspeed "https://raw.githubusercontent.com/Paper890/mysc/main/update/menu-speedtest.sh" && chmod +x /usr/bin/mspeed
 wget -q -O /usr/bin/mbandwith "https://raw.githubusercontent.com/Paper890/mysc/main/update/menu-bandwith.sh" && chmod +x /usr/bin/mbandwith
 echo -e " [INFO] Update Successfully"
 sleep 2
 rm -f update.sh
 exit
-
