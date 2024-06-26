02fa0fe13a8e7c6c5ef57eb34926e2e664c25d7c
Paper890 <140361640+Paper890@users.noreply.github.com>
2024-06-26 15:17:01 +0800

    Update menu-backup.sh

--- update/menu-backup.sh
+++ update/menu-backup.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
+
+# Fetch the date from the server
 dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
-biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
+biji=$(date +"%Y-%m-%d" -d "$dateFromServer")
+
 ###########- COLOR CODE -##############
-colornow=$(cat /etc/ssnvpn/theme/color.conf)
+colornow=$(cat /etc/ssnvpn/theme/color.conf 2>/dev/null || echo "default")  # Fallback to "default" if the file doesn't exist
 NC="\e[0m"
-RED="\033[0;31m" 
+RED="\033[0;31m"
 COLOR1="\033[1;36m"
-COLBG1="\e[1;97;101m"                 
+COLBG1="\e[1;97;101m"
+
 # // Export Color & Information
 export RED='\033[0;31m'
 export GREEN='\033[0;32m'
@@ -21,18 +25,18 @@ clear
 echo -e "\033[1;36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo -e " ${COLBG1}            • MENU BACKUP •             ${NC} "
 echo -e "\033[1;36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "\033[1;93m〔⎆〕 ${grenbo}1.${NC} \033[0;36mBackup VPS${NC}"
-echo -e "\033[1;93m〔⎆〕 ${grenbo}2.${NC} \033[0;36mRestore VPS${NC}"
-echo -e "\033[1;93m〔⎆〕 ${grenbo}3.${NC} \033[0;36mInstall Auto Backup BOT${NC}"
-echo -e "\033[1;93m〔⎆〕 ${grenbo}0.${NC} \033[0;36mMenu${NC}"
+echo -e "\033[1;93m〔⎆〕 ${GREEN}1.${NC} \033[0;36mBackup VPS${NC}"
+echo -e "\033[1;93m〔⎆〕 ${GREEN}2.${NC} \033[0;36mRestore VPS${NC}"
+echo -e "\033[1;93m〔⎆〕 ${GREEN}3.${NC} \033[0;36mInstall Auto Backup BOT${NC}"
+echo -e "\033[1;93m〔⎆〕 ${GREEN}0.${NC} \033[0;36mMenu${NC}"
 echo -e "\033[1;36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo -e ""
 read -p " Select menu :  "  opt
 echo -e ""
 case $opt in
-01 | 1) clear ; backup ;;
-02 | 2) clear ; restore ;;
-03 | 3) clear ; wget -q https://raw.githubusercontent.com/Paper890/mysc/main/backup/auto.sh && chmod +x auto.sh && ./auto.sh ;;
-00 | 0) clear ; menu ;;
-*) clear ; menu-backup ;;
+  1) clear ; backup ;;
+  2) clear ; restore ;;
+  3) clear ; wget -q https://raw.githubusercontent.com/Paper890/mysc/main/backup/auto.sh && chmod +x auto.sh && ./auto.sh ;;
+  0) clear ; menu ;;
+  *) clear ; menu-backup ;;
 esac
