e8a3d86d47946d93fbdbc427f62e90caa882cb44
Paper890 <140361640+Paper890@users.noreply.github.com>
2024-07-12 15:59:05 +0800

    Update setup_bot.sh

--- bot/setup_bot.sh
+++ bot/setup_bot.sh
@@ -376,42 +376,16 @@ EOF
 
 ###### Install Module ####
 
-# ssh_new.sh
-wget -q https://raw.githubusercontent.com/Paper890/mysc/bot/ssh_new.sh && chmod +x ssh_new.sh
-
-# vmess_new.sh
-wget -q https://raw.githubusercontent.com/Paper890/mysc/bot/vmess_new.sh && chmod +x vmess_new.sh
-
-# vless_new.sh
-wget -q https://raw.githubusercontent.com/Paper890/mysc/bot/vless_new.sh && chmod +x vless_new.sh
-
-# trojan_new.sh
-wget -q https://raw.githubusercontent.com/Paper890/mysc/bot/trojan_new.sh && chmod +x trojan_new.sh
-
-# ssh_delete.sh
-wget -q https://raw.githubusercontent.com/Paper890/mysc/bot/ssh_delete.sh && chmod +x ssh_delete.sh
-
-# vmess_delete.sh
-wget -q https://raw.githubusercontent.com/Paper890/mysc/bot/vmess_delete.sh && chmod +x vmess_delete.sh
-
-# vless_delete.sh
-wget -q https://raw.githubusercontent.com/Paper890/mysc/bot/vless_delete.sh && chmod +x vless_delete.sh
-
-# trojan_delete.sh
-wget -q https://raw.githubusercontent.com/Paper890/mysc/bot/trojan_delete.sh && chmod +x trojan_delete.sh
-
-# ssh_renew.sh
-wget -q https://raw.githubusercontent.com/Paper890/mysc/bot/ssh_renew.sh && chmod +x ssh_renew.sh
-
-# vmess_renew.sh
-wget -q https://raw.githubusercontent.com/Paper890/mysc/bot/vmess_renew.sh && chmod +x vmess_renew.sh
-
-# vless_renew.sh
-wget -q https://raw.githubusercontent.com/Paper890/mysc/bot/vless_renew.sh && chmod +x vless_renew.sh
-
-# trojan_renew.sh
-wget -q https://raw.githubusercontent.com/Paper890/mysc/bot/trojan_renew.sh && chmod +x trojan_renew.sh
-
+wget https://raw.githubusercontent.com/Paper890/mysc/main/bot/vmess_renew.sh && chmod +x vmess_renew.sh
+wget https://raw.githubusercontent.com/Paper890/mysc/main/bot/vmess_new.sh && chmod +x vmess_new.sh
+wget https://raw.githubusercontent.com/Paper890/mysc/main/bot/vmess_delete.sh && chmod +x vmess_delete.sh
+wget https://raw.githubusercontent.com/Paper890/mysc/main/bot/vmess_renew.sh && chmod +x vmess_renew.sh
+wget https://raw.githubusercontent.com/Paper890/mysc/main/bot/vless_new.sh && chmod +x vless_new.sh
+wget https://raw.githubusercontent.com/Paper890/mysc/main/bot/vless_delete.sh && chmod +x vless_delete.sh
+wget https://raw.githubusercontent.com/Paper890/mysc/main/bot/vless_renew.sh && chmod +x vless_renew.sh
+wget https://raw.githubusercontent.com/Paper890/mysc/main/bot/trojan_delete.sh && chmod +x trojan_delete.sh
+wget https://raw.githubusercontent.com/Paper890/mysc/main/bot/trojan_new.sh && chmod +x trojan_new.sh
+wget https://raw.githubusercontent.com/Paper890/mysc/main/bot/trojan_renew.sh && chmod +x trojan_renew.sh
 
 # Buat file service systemd
 cat <<EOF > /etc/systemd/system/sanbot.service
