e145a587f84d20a42fc246bc1b4a6dcdf6ed9b92
Paper890 <140361640+Paper890@users.noreply.github.com>
2024-08-06 15:31:56 +0800

    Update menu-backup.sh

--- update/menu-backup.sh
+++ update/menu-backup.sh
@@ -1,38 +1,36 @@
 #!/bin/bash
-MYIP=$(wget -qO- ipv4.icanhazip.com);
-echo "Checking VPS"
+dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
+biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
+###########- COLOR CODE -##############
+colornow=$(cat /etc/ssnvpn/theme/color.conf)
+NC="\e[0m"
+RED="\033[0;31m" 
+COLOR1="\033[1;36m"
+COLBG1="\e[1;97;101m"                 
+# // Export Color & Information
+export RED='\033[0;31m'
+export GREEN='\033[0;32m'
+export YELLOW='\033[0;33m'
+export BLUE='\033[0;34m'
+export PURPLE='\033[0;35m'
+export CYAN='\033[0;36m'
+export LIGHT='\033[0;37m'
+export NC='\033[0m'
+
 clear
-echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "\E[0;100;33m          • SYSTEM MENU •          \E[0m"
-echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e "\033[1;36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e " ${COLBG1}            • MENU BACKUP •             ${NC} "
+echo -e "\033[1;36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e "\033[1;93m〔⎆〕 ${grenbo}1.${NC} \033[0;36mBackup VPS${NC}"
+echo -e "\033[1;93m〔⎆〕 ${grenbo}2.${NC} \033[0;36mRestore VPS${NC}"
+echo -e "\033[1;93m〔⎆〕 ${grenbo}0.${NC} \033[0;36mMenu${NC}"
+echo -e "\033[1;36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo -e ""
-echo -e " [\e[36m•1\e[0m] Panel Domain"
-echo -e " [\e[36m•2\e[0m] Speedtest VPS"
-echo -e " [\e[36m•3\e[0m] Set Auto Reboot"
-echo -e " [\e[36m•4\e[0m] Restart All Service"
-echo -e " [\e[36m•5\e[0m] Cek Bandwith"
-echo -e " [\e[36m•6\e[0m] Install TCP BBR"
-echo -e " [\e[36m•7\e[0m] DNS CHANGER"
-echo -e " [\e[36m•8\e[0m] Install Autobackup Bot"
-echo -e ""
-echo -e " [\e[31m•0\e[0m] \e[31mBACK TO MENU\033[0m"
-echo -e   ""
-echo -e   "Press x or [ Ctrl+C ] • To-Exit"
-echo -e   ""
-echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e ""
-read -p " Select menu : " opt
+read -p " Select menu :  "  opt
 echo -e ""
 case $opt in
-1) clear ; m-domain ; exit ;;
-2) clear ; speedtest ; exit ;;
-3) clear ; auto-reboot ; exit ;;
-4) clear ; restart ; exit ;;
-5) clear ; bw ; exit ;;
-6) clear ; m-tcp ; exit ;;
-7) clear ; m-dns ; exit ;;
-8) clear ; wget -q https://raw.githubusercontent.com/Paper890/V1/main/menu/auto.sh && chmod +x auto.sh && ./auto.sh ;;
-0) clear ; menu ; exit ;;
-x) exit ;;
-*) echo -e "" ; echo "Anda salah tekan" ; sleep 1 ; m-system ;;
+01 | 1) clear ; backup ;;
+02 | 2) clear ; restore ;;
+00 | 0) clear ; menu ;;
+*) clear ; menu-backup ;;
 esac
