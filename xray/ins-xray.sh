34b763c7b358c6a445e49e743f6af510fbb18692
Paper890 <140361640+Paper890@users.noreply.github.com>
2024-10-12 20:01:26 +0800

    Update ins-xray.sh

--- xray/ins-xray.sh
+++ xray/ins-xray.sh
@@ -353,6 +353,7 @@ cat > /etc/xray/config.json << END
 END
 rm -rf /etc/systemd/system/xray.service.d
 cat <<EOF> /etc/systemd/system/xray.service
+[Unit]
 Description=Xray Service
 Documentation=https://github.com/xtls
 After=network.target nss-lookup.target
